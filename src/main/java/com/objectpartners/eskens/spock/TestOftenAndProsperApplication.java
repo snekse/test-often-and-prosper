bd16126b3cbf96799159b0a48e42d774a2d81210
Derek Eskens <derek@Dereks-MacBook-Pro.local>
2017-04-13 17:29:20 -0500

    Initial project setup

---
+++ src/main/java/com/objectpartners/eskens/spock/TestOftenAndProsperApplication.java
@@ -0,0 +1,12 @@
+package com.objectpartners.eskens.spock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOftenAndProsperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestOftenAndProsperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/objectpartners/eskens/spock/TestOftenAndProsperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.objectpartners.eskens.spock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestOftenAndProsperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
