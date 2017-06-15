8b341ded87cd34bf5b5c232e40f6d2438903dab3
Derek Eskens <derek@Dereks-MBP.lan>
2017-06-15 11:27:43 -0500

    Fix some formatting in the Spock test

--- src/test/groovy/com/objectpartners/eskens/spock/services/CrewRecruiterServiceSpockTest.groovy
+++ src/test/groovy/com/objectpartners/eskens/spock/services/CrewRecruiterServiceSpockTest.groovy
@@ -11,7 +11,6 @@ import spock.lang.Unroll
 
 import static java.lang.Integer.MAX_VALUE
 
-
 class CrewRecruiterServiceSpockTest extends Specification implements CommonCrew {
 
     @Subject CrewRecruiterService crewRecruiterService
@@ -21,13 +20,13 @@ class CrewRecruiterServiceSpockTest extends Specification implements CommonCrew 
     void "SelectForBudget spends budget in crew priority order and avoids unnecessary calls to payroll service"() {
 
         when: 'We have enough to pay for Kirk and Spock'
-        def crew = crewRecruiterService.selectForBudget(200000, ENTIRE_CREW)
+        def crew = crewRecruiterService.selectForBudget(200_000, ENTIRE_CREW)
 
         then: 'We only make our expensive external system salary calls in the order we expect - first add Kirk'
-        1 * payrollService.getSalary(KIRK) >> 100000
+        1 * payrollService.getSalary(KIRK) >> 100_000
 
         then: 'add Spock'
-        1 * payrollService.getSalary(SPOCK) >> 100000
+        1 * payrollService.getSalary(SPOCK) >> 100_000
 
         then: 'no more calls to that service since we have depleted our budget'
         0 * payrollService.getSalary(_)
@@ -37,7 +36,6 @@ class CrewRecruiterServiceSpockTest extends Specification implements CommonCrew 
     }
 
     @SuppressWarnings("GroovyAssignabilityCheck")
-
     @Unroll("A budget of #salaryCap pays for #expectedCrew")
     void "SelectForBudget adds crew in priority order until budget depleted"() {
         given: 'a flat rate of 100000 per member'
@@ -52,10 +50,10 @@ class CrewRecruiterServiceSpockTest extends Specification implements CommonCrew 
         where:
         salaryCap   || expectedCrew
         0           || []
-        (100000-1)  || []
-        100000      || [KIRK]
-        100001      || [KIRK]
-        200000      || [KIRK, SPOCK]
+        (100_000-1) || []
+        100_000     || [KIRK]
+        100_001     || [KIRK]
+        200_000     || [KIRK, SPOCK]
         MAX_VALUE   || ENTIRE_CREW
     }
 
@@ -66,10 +64,10 @@ class CrewRecruiterServiceSpockTest extends Specification implements CommonCrew 
         def crew = crewRecruiterService.selectForBudget(200000, ENTIRE_CREW).findAll { it != SPOCK }
 
         then: 'We only make our expensive external system salary calls in the order we expect - first add Kirk'
-        1 * payrollService.getSalary(KIRK) >> 100000
+        1 * payrollService.getSalary(KIRK) >> 100_000
 
         then: 'add Spock'
-        1 * payrollService.getSalary(SPOCK) >> 100000
+        1 * payrollService.getSalary(SPOCK) >> 100_000
 
         then: 'no more calls to that service since we have depleted our budget'
         0 * payrollService.getSalary(_)
